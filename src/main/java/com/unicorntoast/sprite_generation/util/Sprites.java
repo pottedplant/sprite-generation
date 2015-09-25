25f64c110789e2c007503b947abd401727dc1bb3
m.woschank@ixolit.com <m.woschank@ixolit.com>
2015-09-25 12:12:21 +0200

    to png convenience method

--- src/main/java/com/unicorntoast/sprite_generation/util/Sprites.java
+++ src/main/java/com/unicorntoast/sprite_generation/util/Sprites.java
@@ -3,25 +3,32 @@ package com.unicorntoast.sprite_generation.util;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 
 import com.unicorntoast.sprite_generation.gen.ImageDef;
 import com.unicorntoast.sprite_generation.gen.Placement;
 import com.unicorntoast.sprite_generation.gen.Sheet;
 
 public abstract class Sprites {
-	
+
 	public static <T> BufferedImage create(Sheet<T> sheet,ImageProvider<T> imageProvider) throws IOException {
 		BufferedImage image = new BufferedImage(sheet.width,sheet.height,BufferedImage.TYPE_4BYTE_ABGR);
 		fill(image,sheet.placements,imageProvider);
 		return image;
 	}
-	
+
 	public static <T> void fill(BufferedImage dst,List<Placement<T>> placements,ImageProvider<T> imageProvider) throws IOException {
 		Verify.notNull(dst);
 		Verify.notNull(imageProvider);
-		
+
 		Graphics2D graphics = dst.createGraphics();
 		try {
 			for(Placement<T> placement:placements) {
@@ -32,7 +39,27 @@ public abstract class Sprites {
 			graphics.dispose();
 		}
 	}
-	
+
+	public static byte[] pngByteArray(BufferedImage image) throws IOException {
+		Iterator<ImageWriter> imageWriters = ImageIO.getImageWritersByMIMEType("image/png");
+		try {
+			ImageWriter imageWriter = imageWriters.next();
+			try {
+				ByteArrayOutputStream stream = new ByteArrayOutputStream();
+				try(ImageOutputStream imageStream = new MemoryCacheImageOutputStream(stream)) {
+					imageWriter.setOutput(imageStream);
+					imageWriter.write(image);
+				}
+				return stream.toByteArray();
+			} finally {
+				imageWriter.dispose();
+			}
+		} finally {
+			while(imageWriters.hasNext())
+				imageWriters.next().dispose();
+		}
+	}
+
 	@FunctionalInterface
 	public static interface ImageProvider<T> {
 		Image image(ImageDef<T> source) throws IOException;
